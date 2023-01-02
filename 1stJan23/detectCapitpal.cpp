975ae99775534da3497b5c6a78a005beb93aee7b
Tapas Mondal <94737788+Tapas071@users.noreply.github.com>
2023-01-02 23:44:08 +0530

    this is the commit on detect CapitalUse

---
+++ 1stJan23/detectCapitpal.cpp
@@ -0,0 +1,74 @@
+//-----Author - Tapas Mondal-------
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+#define int             long long
+#define pb              push_back
+#define ppb             pop_back
+#define pf              push_front
+#define ppf             pop_front
+#define all(x)          (x).begin(),(x).end()
+#define uniq(v)         (v).erase(unique(all(v)),(v).end())
+#define sz(x)           (int)((x).size())
+#define ff              first
+#define ss              second
+#define pii             pair<int, int>
+#define rep(i,a,b)      for(int i =a;i<b;i++)
+#define mem1(a)         memset(a,-1,sizeof(a))
+#define mem0(a)         memset(a,0,sizeof(a))
+#define ppc             __builtin_popcount
+#define ppcll           __builtin_popcountll
+ 
+typedef vector<int>vi;
+typedef vector<string>vs;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+ 
+const long long  INF=1e18;
+const int32_t    M=1e9+7;
+const int32_t    MM=998244353;
+ 
+// Code begins from here->
+
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        if(word.size()==1)return true;
+        string temp= word;
+        transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
+        if( word == temp) return true;
+        transform(temp.begin(), temp.end(), temp.begin(),:: toupper);
+        if( word == temp)return true;
+
+        for( int i =1; i< word.size(); i++)
+        {
+            if( word[i]== toupper(word[i])) return false;
+        }
+        return true;
+
+     
+    }
+};
+
+ 
+void solve(){
+ 
+
+ 
+}
+ 
+signed main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+int t=1;
+cin>>t;
+while(t--)
+{
+ 
+solve();
+ 
+}
+return 0;
+}
